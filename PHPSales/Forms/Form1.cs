384810d8d241adecea81890eb60af4578b9c9e78
Brett Sutton <bsutton@noojee.com.au>
2020-08-17 21:41:27 +1000

    implementing Sqlite API for project to make usage extemely simple if we choose to go with sqlite

---
+++ PHPSales/Forms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PHPSales
+{
+    public partial class Form1 : Form
+    {
+        public Button button1;
+
+        public Form1()
+        {
+            button1 = new Button();
+            button1.Size = new Size(100, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            Controls.Add(button1);
+            button1.Click += button1_click;
+            InitializeComponent();
+        }
+
+        private void button1_click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Inserted record");
+            SqliteAPI.SqliteAPI.InsertRecord(name:"testItem", value: 4, "2001-02-12" );
+        }
+    }
+}
---
+++ PHPSales/SqliteAPI/SqliteAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace PHPSales.SqliteAPI
+{
+    public class SqliteAPI
+    {
+        private static readonly string cs = "Data Source=:memory:";
+        private static SQLiteConnection con = new SQLiteConnection(cs);
+        private static SQLiteCommand cmd = new SQLiteCommand(con);
+
+        public static void InitDB()
+        {
+            con.Open();
+
+
+            cmd.CommandText = "DROP TABLE IF EXISTS tblRecords";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE tblRecords(id INTEGER PRIMARY KEY,
+                    itemName TEXT, itemValue INT, saleDate DATE )";
+            cmd.ExecuteNonQuery();
+            
+            cmd.CommandText = "INSERT INTO tblRecords(itemName, itemValue, saleDate) VALUES('name',52642, '2001-02-12')";
+            cmd.ExecuteNonQuery();
+            
+            
+        }
+
+        public static void InsertRecord(string name, int value, string date)
+        {
+            cmd.CommandText = "INSERT INTO tblRecords(itemName, itemValue, saleDate) VALUES(@name,@value,@date)";
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@value", value);
+            cmd.Parameters.AddWithValue("@date", date);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
