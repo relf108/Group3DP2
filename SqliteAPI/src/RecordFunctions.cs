de2469a9fd2b8764c0ef98d37b118716c87c606d
Ruan Swanepoel <40282616+RewForeN@users.noreply.github.com>
2020-09-08 14:27:53 +1000

    Moved SqliteAPI to seperate project and organised project files

---
+++ SqliteAPI/src/RecordFunctions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Text;
+using SqliteAPI.Objects;
+
+namespace SqliteAPI
+{
+    public class RecordFunctions
+    {
+        public static void InsertRecord(string name, double value, string date)
+        {
+            using var cmd = new SQLiteCommand(SqliteAPI.Con);
+            cmd.CommandText = "INSERT INTO tblRecords(itemName, itemValue, saleDate) VALUES(@name,@value,@date)";
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@value", value);
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void DeleteRecord(int primaryKey)
+        {
+            using var cmd = new SQLiteCommand(SqliteAPI.Con);
+            cmd.CommandText = "DELETE FROM tblRecords WHERE id = " + primaryKey;
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void EditRecord(int targetPrimaryKey, string newName, double newValue, string newDate)
+        {
+            using var cmd = new SQLiteCommand(SqliteAPI.Con);
+            cmd.CommandText = "UPDATE tblRecords SET " +
+                              "itemName= '" + newName + "'" +
+                              ", itemValue= '" + newValue + "'" +
+                              ", saleDate= '" + newDate + "'" +
+                              " WHERE id=" + targetPrimaryKey;
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void PrintRows()
+        {
+            var stm = "SELECT * FROM tblRecords";
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            while (rdr.Read())
+                Console.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)} {rdr.GetDouble(2)} {rdr.GetString(3)}");
+        }
+
+        public static List<SaleRecord> listRows()
+        {
+            var list = new List<SaleRecord>();
+            var stm = "SELECT * FROM tblRecords";
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            while (rdr.Read())
+                list.Add(new SaleRecord(rdr.GetInt32(0), rdr.GetString(1),
+                    rdr.GetDouble(2), rdr.GetString(3)));
+            return list;
+        }
+
+        public static SaleRecord getRecordByID(int primaryKey)
+        {
+            var stm = "SELECT * FROM tblRecords WHERE id = " + primaryKey;
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            return new SaleRecord(rdr.GetInt32(0), rdr.GetString(1), rdr.GetDouble(2), rdr.GetString(3));
+        }
+        public static void exportTable(string path, string filename)
+        {
+            var csv = new StringBuilder();
+            var records = listRows();
+            csv.AppendLine("id, item name, item value, sale date");
+            foreach (var record in records)
+                csv.AppendLine($"{record.id}, {record.itemName}, {record.itemValue}, {record.saleDate}");
+            try
+            {
+                File.WriteAllText($"{path}\\{filename}.csv", csv.ToString());
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("An exception occured: The path specified could not be found. Make sure your capitalisation is correct.");
+                Console.WriteLine(e.Source);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                
+                Console.WriteLine("An exception occurredYou do not have sufficient permissions to write to the specified path. " +
+                                  "Try using the path C:\\Users\\YourUsername\\Documents");
+                Console.WriteLine(e.Source);
+            }
+        }
+
+        public static void DropTable()
+        {
+            //would be used for db rollover if we get to that.
+            using var cmd = new SQLiteCommand(SqliteAPI.Con);
+            cmd.CommandText = "DROP TABLE IF EXISTS tblRecords";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ SqliteAPI/src/UserFunctions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using SqliteAPI.Objects;
+
+namespace SqliteAPI
+{
+    public class UserFunctions
+    {
+        public static void InsertUser(string name, bool role)
+        {
+            if (getUserByUsername(name) == null)
+            {
+                using var cmd = new SQLiteCommand(SqliteAPI.Con);
+                cmd.CommandText = "INSERT INTO tblUser(userName, userRole) VALUES(@name,@role)";
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@role", role);
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                throw new Exception("An exception occured: That username is already taken");
+            }
+            
+        }
+
+        public static void DeleteUser(string userName)
+        {
+            if (getUserByUsername(userName) != null)
+            {
+                using var cmd = new SQLiteCommand(SqliteAPI.Con);
+                cmd.CommandText = "DELETE FROM tblUser WHERE userName = " + userName;
+                cmd.ExecuteNonQuery();
+            }
+            throw new Exception("An exception occured: The user you're trying to delete does not exist");
+        }
+        
+        public static void EditUser(int targetPrimaryKey, string newName, double newRole)
+        {
+            using var cmd = new SQLiteCommand(SqliteAPI.Con);
+            cmd.CommandText = "UPDATE tblUser SET " +
+                              "userName= '" + newName + "'" +
+                              ", userRole= '" + newRole + "'" +
+                              " WHERE id=" + targetPrimaryKey;
+            cmd.ExecuteNonQuery();
+        }
+        
+        public static List<User> listRows()
+        {
+            var list = new List<User>();
+            var stm = "SELECT * FROM tblUser";
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            while (rdr.Read())
+                list.Add(new User(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2),
+                    rdr.GetBoolean(3)));
+            return list;
+        }
+
+        public static User getUserByID(int primaryKey)
+        {
+            var stm = "SELECT * FROM tblUser WHERE id = " + primaryKey;
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                return new User(rdr.GetInt32(0), rdr.GetString(1),rdr.GetString(2), rdr.GetBoolean(3));
+            }
+
+            return null;
+        }
+        
+        public static User getUserByUsername(string userName)
+        {
+            var stm = "SELECT * FROM tblUser WHERE userName = " + userName;
+            using var readercmd = new SQLiteCommand(stm, SqliteAPI.Con);
+            using var rdr = readercmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                return new User(rdr.GetInt32(0), rdr.GetString(1),rdr.GetString(2), rdr.GetBoolean(3));
+            }
+            return null;
+            }
+
+        public static bool authUser(string userName, string userPassword)
+        {
+            if(getUserByUsername(userName).userPassword.Equals(userPassword))
+            {
+                return getUserByUsername(userName).userRole;
+            }
+            throw new Exception("An exception occured: The user does not exist or the username and password do not match");
+        }
+    }
+}
