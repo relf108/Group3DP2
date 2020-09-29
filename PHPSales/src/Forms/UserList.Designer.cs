1ab19a551d6c321dbd9028cdfef8ed94a5f60e6d
SCOTT MATTHEWS <6817475@student.swin.edu.au>
2020-09-29 23:22:14 +1000

    UserForm Edit
    added a password field for adding a user

--- PHPSales/src/Forms/UserList.Designer.cs
+++ PHPSales/src/Forms/UserList.Designer.cs
@@ -34,6 +34,9 @@
             this.AddButton = new System.Windows.Forms.Button();
             this.RemoveButton = new System.Windows.Forms.Button();
             this.AdminCheck = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BackButton
@@ -58,12 +61,12 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(410, 45);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(350, 23);
+            this.textBox1.Size = new System.Drawing.Size(292, 23);
             this.textBox1.TabIndex = 1;
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(413, 89);
+            this.AddButton.Location = new System.Drawing.Point(410, 120);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(167, 36);
             this.AddButton.TabIndex = 2;
@@ -72,7 +75,7 @@
             // 
             // RemoveButton
             // 
-            this.RemoveButton.Location = new System.Drawing.Point(586, 89);
+            this.RemoveButton.Location = new System.Drawing.Point(587, 121);
             this.RemoveButton.Name = "RemoveButton";
             this.RemoveButton.Size = new System.Drawing.Size(173, 35);
             this.RemoveButton.TabIndex = 3;
@@ -89,11 +92,40 @@
             this.AdminCheck.Text = "Admin?";
             this.AdminCheck.UseVisualStyleBackColor = true;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(722, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(725, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
             // UserList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.AdminCheck);
             this.Controls.Add(this.RemoveButton);
             this.Controls.Add(this.AddButton);
@@ -115,5 +147,8 @@
         private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.Button RemoveButton;
         private System.Windows.Forms.CheckBox AdminCheck;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- PHPSales/src/Forms/UserList.cs
+++ PHPSales/src/Forms/UserList.cs
@@ -14,5 +14,10 @@ namespace PHPSales.src.Forms
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
